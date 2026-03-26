009974e728f7cb47b77096a136c4b4c1c2fcda22
Nguyễn Phi Long <xanh3248@gmail.com>
2026-03-26 09:13:11 +0700

    init

---
+++ src/main/java/com/nitrotech/api/NitrotechApiApplication.java
@@ -0,0 +1,13 @@
+package com.nitrotech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitrotechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitrotechApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitrotech/api/NitrotechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitrotech.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitrotechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
